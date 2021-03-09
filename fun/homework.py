e11ade134f2c423deae4fa1e68593f4c2e7780f7
dupertba <74461690+dupertba@users.noreply.github.com>
2021-03-08 22:09:48 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -7,45 +7,23 @@ def find_greatest_number(incoming_list):
 
 
 
-    
-    """
-    Required parameter, incoming_list, should be a list.
-    
-    Find the largest number in the list.
-    """
-    pass
-
 
 def find_least_number(incoming_list):
     print(min(incoming_list))
     return min(incoming_list)
   
 
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the smallest/least number in the list.
-    """
-    pass
 
 
 def add_list_numbers(incoming_list):
     print(sum(incoming_list))
     return sum(incoming_list)
-    
-    
-    """
-    Required parameter, incoming_list, should be a list.
-    Add all the values together and return it.
-    """
 
-    pass
+
 
 
 def longest_value_key(incoming_dict):
     biggest_capital = max(incoming_dict, key = lambda x: len(set(biggest_capital[x])))
     return biggest_capital
-    """
-    Required parameter, incoming_dict, should be a dict.
-    Find the KEY that has a value with the highest length, use the len() function
-    """
-    pass
+
+
