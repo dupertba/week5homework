ac61e1728215387f2066a29509ff6f53e88cecab
dupertba <74461690+dupertba@users.noreply.github.com>
2021-03-08 22:26:26 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -2,21 +2,18 @@
 
 
 def find_greatest_number(incoming_list):
-    print(max(incoming_list))
     return max(incoming_list)
 
 
 
 
 def find_least_number(incoming_list):
-    print(min(incoming_list))
     return min(incoming_list)
   
 
 
 
 def add_list_numbers(incoming_list):
-    print(sum(incoming_list))
     return sum(incoming_list)
 
 
