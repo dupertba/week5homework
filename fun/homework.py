323acb4920e681133fbea4a816ce823cc2e13827
dupertba <74461690+dupertba@users.noreply.github.com>
2021-03-08 20:28:04 -0500

    Update homework.py
    update

--- fun/homework.py
+++ fun/homework.py
@@ -3,8 +3,8 @@
 
 def find_greatest_number(incoming_list):
     incoming_list = [1,2,3,4,5,6,7,8,9,10]
-    max(incoming_list)
-    print(incoming_list)
+    return max(incoming_list)
+    
 
     """
     Required parameter, incoming_list, should be a list.
@@ -16,8 +16,8 @@ def find_greatest_number(incoming_list):
 
 def find_least_number(incoming_list):
     incoming_list = [1,2,3,4,5,6,7,8,9,10]
-    min(incoming_list)
-    print(incoming_list)
+    return min(incoming_list)
+    
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
@@ -27,8 +27,8 @@ def find_least_number(incoming_list):
 
 def add_list_numbers(incoming_list):
     incoming_list = [1,2,3,4,5,6,7,8,9,10]
-    sum(incoming_list)
-    print(incoming_list)
+    return sum(incoming_list)
+    
     
     """
     Required parameter, incoming_list, should be a list.
@@ -43,11 +43,10 @@ def longest_value_key(incoming_dict):
     'Boston':'Massachusetts',
     'Raleigh': 'North Carolina'}
 
-    biggest_capital = max(incoming_dict, key = lambda x: len(set(biggest_capital[x])))
-    print(biggest_capital)
+    return biggest_capital = max(incoming_dict, key = lambda x: len(set(biggest_capital[x])))
+    
     """
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
     pass
-print("hello")
