6abb9fd19256d4f06e0f8773bdef00bcaf80dff3
dupertba <74461690+dupertba@users.noreply.github.com>
2021-03-16 11:23:16 -0400

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -2,25 +2,33 @@
 
 
 def find_greatest_number(incoming_list):
-    return max(incoming_list)
+    great_num = max(incoming_list)
+    return great_num
 
 
 
 
 def find_least_number(incoming_list):
-    return min(incoming_list)
+    least_num = min(incoming_list)
+    return least_num
   
 
 
 
 def add_list_numbers(incoming_list):
-    return sum(incoming_list)
+
+    if incoming_list:
+        sum_list = sum(incoming_list)
+    else
+        sum_list = 0
+    return sum_list
 
 
 
 
 def longest_value_key(incoming_dict):
-    return len(max(incoming_dict, key=len))
+    long_val = len(max(incoming_dict, key=len))
+    return long_val
     
 
 
